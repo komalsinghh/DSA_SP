39735838b65e7bb8d98a07ff47e55a663e9a44cf
Komal Singh <singhkomal3669@gmail.com>
2022-10-26 22:04:50 +0530

    done

--- String/src/AnagramSearch.java
+++ String/src/AnagramSearch.java
@@ -29,16 +29,27 @@ public class AnagramSearch {
 		return false;
 	}
 
-	/*
-	 * static boolean areAnagram(String pat, String txt,int i) { int[] count=new
-	 * int[CHAR]; for(int j=0;j<pat.length();j++){ count[pat.charAt(j)]++;
-	 * count[txt.charAt(i+j)]--; } for(int j=0;j<CHAR;j++){ if(count[j]!=0)return
-	 * false; } return true; }
-	 * 
-	 * static boolean isPresent(String txt,String pat){ int n=txt.length(); int
-	 * m=pat.length(); for(int i=0;i<=n-m;i++){ if(areAnagram(pat,txt,i))return
-	 * true; } return false; }
-	 */
+	/*static boolean areAnagram(String pat, String txt,int i) 
+    { 
+        int[] count=new int[CHAR];
+    for(int j=0;j<pat.length();j++){
+        count[pat.charAt(j)]++;
+        count[txt.charAt(i+j)]--;
+    }
+    for(int j=0;j<CHAR;j++){
+        if(count[j]!=0)return false;
+    }
+    return true;
+    } 
+    
+    static boolean isPresent(String txt,String pat){
+    int n=txt.length();
+    int m=pat.length();
+    for(int i=0;i<=n-m;i++){
+        if(areAnagram(pat,txt,i))return true;
+    }
+    return false;
+    }*/
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter Word");
